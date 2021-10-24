26b22c0d690b0efc0de684571cf8a1d3ebc4b71f
ramesh <ramesh.c@implevisiontech.com>
2021-10-24 16:54:31 +0530

    remove variable with constants

---
+++ app/src/main/java/com/sr/savefileproject/util/ParamsUtils.kt
@@ -0,0 +1,12 @@
+package com.sr.savefileproject.util
+
+/**
+ * Created by ramesh on 22-10-2021
+ */
+object ParamsUtils {
+    const val PATH = "path"
+    const val AUDIO = "audio"
+    const val IMAGE = "image"
+    const val VIDEO = "video"
+    const val APP = "application"
+}
